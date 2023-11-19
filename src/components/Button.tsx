2830d0e0528e3322b47b2578c89316391750dabd
3kezoh <3kezoh@gmail.com>
2023-11-19 17:46:34 +0100

    feat: update button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,9 +5,16 @@ type ButtonProps = React.ComponentPropsWithoutRef<"button">;
 export function Button(props: ButtonProps) {
   const { className, disabled, ...rest } = props;
 
-  const buttonClassName = cn("border border-black px-2", className, {
-    "cursor-not-allowed": disabled,
-  });
+  const buttonClassName = cn(
+    "border border-black px-2",
+    className,
+    {
+      "transition-colors hover:text-white hover:bg-black": !disabled,
+    },
+    {
+      "cursor-not-allowed bg-gray-200 text-gray-400 border-gray-400": disabled,
+    },
+  );
 
   return (
     <button
