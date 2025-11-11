a0a2321e4ed938ae0d1c759add52085ddeb76248
3kezoh <stanleycrico@duck.com>
2025-11-11 22:19:39 +0100

    fix tsconfig for vite and vitest configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import type { UserConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
 	plugins: [react(), tailwindcss()],
 	server: {
 		port: 3000,
 	},
-});
+} satisfies UserConfig;
